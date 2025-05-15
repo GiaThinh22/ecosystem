8801281d4af5b036a3d60cb78c191fa9e133d726
GiaThinh22 <nguyen.gia.thinh220809@gmail.com>
2025-05-15 10:05:31 +0800

    commit 2 (world  generation using seeds)

--- plants.js
+++ plants.js
@@ -1,20 +1,20 @@
 class cattail{
     constructor(x,y,a,h){
         this.pos = createVector(x,y);
-        this.a = a;
+        this.angle = a;
         this.sway;
-        this.h = h;
+        this.height = h + map(noise(x), 0, 1, -10,10);
     }
     update(){
         push();
         translate(this.pos.x, this.pos.y);
-        this.sway = sin(this.a)*5; 
+        this.sway = sin(this.angle)*5; 
         rotate(radians(this.sway));
         fill(60, 120, 60);
-        rect(-2, this.h, 4, 150);
+        rect(-2, this.height, 4, 150);
         fill(139, 69, 19);
-        rect(-5, this.h-20, 10, 30, 5);
-        this.a+=random(0.01,0.04);
+        rect(-5, this.height-20, 10, 30, 5);
+        this.angle+=random(0.01,0.04);
         pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -6,20 +6,24 @@ let G; // gravity
 let analyseMode = false,stopPressed = true,blackCoverAlpha = 0;//analyse mode components
 let analyseArrow,analyseDatas = [];
 let eatCount=0,pointsEarned = 0; // flies eaten counter and points earned
-
+let seedReady = false;
 let seed = "",seedComponents = [];
 let noiseOffsetX = 0.0,noiseOffsetY = 100.0;
+let invalidTimer = 0;
+let worldReady = false;
+//3,6,29,4,20,151,14,46
 //0x-1X-2aa-3y-4YY-5zzz-6ZZ-7NoiseSeed
 /*clouds 	x-type of clouds		X-amount		aa-size
   flies 		y-size				YY-amount
   cattails 	zzz-height			ZZ-amount*/
-
+let seedInputed = []; 
+let input, button;
 
 function generateSeed(){
   seedComponents[0] = round(random(1,3));//cloud type
   seedComponents[1] = round(random(5,9));//cloud amount
   seedComponents[2] = round(random(20,30));//cloud size
-  seedComponents[3] = round(random(2,4));//fly size
+  seedComponents[3] = round(random(3,5));//fly size
   seedComponents[4] = round(random(15,25));//fly amount
   seedComponents[5] = round(random(150,170));//cattail height
   seedComponents[6] = round(random(10,15));//cattail amount
@@ -27,32 +31,153 @@ function generateSeed(){
   noiseSeed(seedComponents[7]);
   seed = str(seedComponents);
 }
+
+function resetEverything(){
+  envi = [], stars = [], clouds = [];// land/liquid/air array, stars array, clouds array
+  cattails = [];// cattails array
+  frogs, flies = [];//frog and flies array
+  day = 0,dc = 2,time,timeString;// day and dayCount and time converted from day
+  G; // gravity
+  analyseMode = false,stopPressed = true,blackCoverAlpha = 0;//analyse mode components
+  analyseArrow,analyseDatas = [];
+  eatCount=0,pointsEarned = 0; // flies eaten counter and points earned
+  seedReady = false;
+  seed = "",seedComponents = [];
+  noiseOffsetX = 0.0,noiseOffsetY = 100.0;
+  invalidTimer = 0;
+  worldReady = false;
+}
+
 function setup() {
   createCanvas(600, 350);
 
   generateSeed();
+  input = createInput('Input or paste a seed');
+  button = createButton("submit");
+  button.mouseClicked(submitted);
+}
 
+function submitted(){
+  
+  let inputList = input.value();
+  seedInputed = inputList.split(',').map(item => item.trim());
+
+  if(seedInputed[0] === "random"){
+    resetEverything();
+    generateSeed();
+    input.value(seedComponents);
+    seedReady = true;
+    return;
+  }
+
+  if (seedInputed.length === 8) {
+    let validSeed =
+      checkInRange(1,3,seedInputed[0]) &&
+      checkInRange(5,9,seedInputed[1]) &&
+      checkInRange(20,30,seedInputed[2]) &&
+      checkInRange(3,5,seedInputed[3]) &&
+      checkInRange(15,25,seedInputed[4]) &&
+      checkInRange(150,170,seedInputed[5]) &&
+      checkInRange(10,15,seedInputed[6]) &&
+      checkInRange(10,99,seedInputed[7]);
+
+    if(validSeed){
+      resetEverything();
+      copyArray(8);
+      input.value(seedComponents);
+      seedReady = true;
+    } else {
+      seedReady = false;
+      seedInputed = [];
+      input.value("");
+      invalidTimer =100;
+
+    }
+  } 
+  else {
+    seedReady = false;
+    seedInputed = [];
+    input.value("");
+    invalidTimer = 100;
+  }
+
+}
+function instructionMessage(){
+  if(invalidTimer<=1){
+    push();
+    textSize(20);
+    fill(255);
+    textAlign(CENTER);
+    text("You control a frog in a swamp",width/2,30);
+    text("T to switch ANALYSE MODE on/off",width/2,60);
+    text("Jumping to flies allows you to convert them to points",width/2,90);
+    text("Flies respawn during the day",width/2,120);
+    text("Q to swap between AUTO MODE and CONTROL MODE",width/2,150);
+    text("A and D to look left and right respectively in CONTROL MODE",width/2,180);
+    text("W to jump in CONTROL MODE",width/2,210);
+    text("S to switch to S***ING MODE and attract flies",width/2,240);
+    text("During S***ING MODE, points are rapidly consumed",width/2,270);
+    fill(0,frameCount%150 + 150,0);
+    text("Input a seed to begin the process",width/2,300);
+    pop();
+  }
+}
+function showInvalidMessage() {
+  push();
+  textSize(20);
+  fill(255);
+  textAlign(CENTER);
+  if(invalidTimer>0){
+    text("Invalid seed", width/2, height/3);
+    text("Type random to generate random seed", width/2, height/2);
+  }
+
+  pop();
+}
+function copyArray(length) {
+  for (let i = 0; i < length; i++) {
+    seedComponents[i] = seedInputed[i];
+  }
+  seed = str(seedComponents);
+}
+function checkInRange(min, max, value) {
+  let num = parseInt(value);
+  return !isNaN(num) && num >= min && num <= max;
+}
+
+function generateWorld(){
   G =  0.5;
-  frogs = new frog(200,100,1.5,3);
-  analyseArrow = new vectorArrows(frogs);
   generateCloud();
-  for(let s = 0; s<30; s++){
-    stars[s] = new STAR(random(0,600),random(0,240),random(0.5,5));
-  }
-  generateCloud();
-  for(let s = 0; s<10; s++){
-    cattails[s] = new cattail(10+7*s,300,random(-5,5),random(-180,-150));
-  }
-  for(let f = 0; f<20;f++){
-    flies[f] = new fly(random(10,80),random(100,200),random(4,6),0.05);
-  }
+  generateStar();
+  generatePlants();
+  generateAnimals();
+  generateEnvironment();
+}
+function generateEnvironment(){
   envi[0] = new env(0,250,350,height,true,false,false);
   envi[1] = new env(350,250,width,height,false,true,false);
   envi[2] = new env(0,0,width,height,false,false,true);
   envi[3] = new env(340,350,width,height,true,false,false);
   envi[4] = new env(350,340,50,10,false,false,false); //jumping pad
 }
-function generateCloud(seed){
+function generateAnimals(){
+  frogs = new frog(200,100,1.5,3);
+  analyseArrow = new vectorArrows(frogs);
+  for(let f = 0; f<seedComponents[4];f++){
+    flies[f] = new fly(random(10,80),random(100,200),seedComponents[3],0.05);
+  }
+}
+function generatePlants(){
+    for(let s = 0; s<seedComponents[6]; s++){
+    cattails[s] = new cattail(10+7*s,300,random(-5,5),-1*seedComponents[5]);
+  }
+}
+function generateStar(){
+  for(let s = 0; s<30; s++){
+    stars[s] = new STAR(random(0,600),random(0,240),random(0.5,5));
+  }
+}
+function generateCloud(){
   for(let c = 0; c<seedComponents[1]; c++){
     let x = noise(noiseOffsetX) * width;
     noiseOffsetX += 5;
@@ -64,13 +189,30 @@ function generateCloud(seed){
   }
 }
 function draw() {
-  background(20, 20, 60);
+
+  if(!seedReady){
+    background(0);
+    instructionMessage();
+      showInvalidMessage();
+  invalidTimer--;
+  }
+
+  if(seedReady == true){
+      showInvalidMessage();
+  invalidTimer--;
+    if(!worldReady){
+        generateWorld();
+        worldReady = true;
+    }
+
+      background(20, 20, 60);
+  
   environment();
   UI();
   checkAnalyse();
   animals();
   mainObjects();
-
+  }
 }
 function mainObjects(){
 
@@ -93,8 +235,14 @@ function animals(){
 }
 function UI(){
   push();
+  if(day>100){
+    fill(0);
+  }
+  else{
+    fill(255);
+  }
   textSize(20);
-  text("Score: "+floor(pointsEarned)+"   Seed:" + seed, 20,30);
+  text("Score: "+floor(pointsEarned)+"    Seed:" + seed, 20,30);
   text(frogs.mode,20,50);
   pop();
 }
